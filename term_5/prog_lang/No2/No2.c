fb4811143422af53c36484d7f69db626958a86e8
supaman4561 <dosu416@gmail.com>
2019-04-21 23:20:08 +0900

    finish No2

--- term_5/prog_lang/No2/No2.c
+++ term_5/prog_lang/No2/No2.c
@@ -1,123 +1,166 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
-//https://www.ied.inf.uec.ac.jp/support/projects/ied-support/wiki/%E6%95%99%E6%9D%90
- 
-#define SUCCESS 1    /* 成功*/ 
-#define FAILURE 0    /* 失敗*/
-#define SIZE 100
- 
-/*---------------------------------------------------------------------------------------*/
- 
-//構造体宣言
-typedef char* string_type;
-typedef int int_type;
+
+#define SUCCESS 1    
+#define FAILURE 0    
+
 typedef struct node_tag {
-    string_type name;       //名前
-    int_type point;         //点数
-    struct node_tag *next;   /* 後続ノードへのポインタ*/
-} node_type;                     /* ノードの型*/
- 
-void print_nodes(node_type *p);
+  struct node_tag *next;
+  char* name;      
+  int point;       
+} node_t;                 
 
+node_t *new_node(char *nm, int pnt, node_t *p);
+int insert_nameorder(char *nm, int pnt, node_t **pp);
+int move_nodes_pointorder(node_t *p_nmodr, node_t **pp_ptodr);
+void show_list(node_t *p);
+void free_list(node_t *p);
 
-//線形リストの初期化
-void initialize(node_type **pp)
-{
-    *pp = NULL;     //先頭ノードなし
-}
- 
-//新しいノードの生成と値の設定
-node_type *new_node(string_type na, int_type po, node_type *p)
-{
-    node_type *temp;
- 
-    temp = (node_type *)malloc(sizeof(node_type));  //メモリ割り当て
-    temp->name = (string_type)malloc(sizeof(char) * SIZE);
-
-    if(temp == NULL){ return NULL; }            //メモリ割り当て失敗
-    else{                       //ノードの各メンバの値の設定
-        strcpy(temp->name, na);
-        temp->point = po;
-        temp->next = p;
-        return temp;
-    }
-}
- 
-//線形リストに名前順で挿入
-int insert(string_type na, int_type po, node_type **pp) //node_type **ppはヘッダのポインタ
-{
-    node_type *temp;
-    temp = new_node(na, po, NULL);    /* 関数new_nodeの呼出し*/
-
-    //挿入すべき場所を探す
-    if(temp == NULL){ return FAILURE; }
-
-    if (*pp == NULL)  {
-        *pp = temp;
-    } else {
-        while((*pp) != NULL && (strcmp(temp->name, (*pp)->name)) > 0 ) {
-            pp = &((*pp)->next);
-        }
-        temp->next = (*pp);
-        *pp = temp;
-    }
-
-    return SUCCESS;
-}
-
-
-/*
-//線形リストの末尾へのノード挿入
-int insert_rear(string_type na, int_type po, node_type **pp) //node_type **ppはヘッダのポインタ
-{
-    node_type *temp;
-    temp = new_node(na, po, NULL);    // 関数new_nodeの呼出し
-    if(temp == NULL){ return FAILURE; }
-    while(*pp != NULL) {    // 末尾のノードまで進める
-       pp = &((*pp)->next);
-    }
-    *pp = temp;
-    return SUCCESS;
-}
-*/
- 
-void print_nodes(node_type *p)
-{
-    while(p != NULL)
-    {
-        printf("Name:%s,Point:%d\n", p->name, p->point); //データの表示
-        p = p->next;
-    }
-}
- 
- 
-/*---------------------------------------------------------------------------------------*/
- 
 int main(int argc, char **argv)
 {
-    FILE *fp;
-    char na[256];
-    int po;
-     
-    node_type *head;
-    initialize(&head);
- 
-    if((fp = fopen(argv[1], "r")) == NULL){
-    perror(argv[1]);
-        exit(1);
+  FILE *fp;
+  node_t *head_nmodr, *head_ptodr;
+  char *name;
+  int point;
+
+  if (argc < 2) {
+    printf("Usage: ./No2 [filename]\n");
+  } else {
+    fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+      printf("Failed to open the file...\n");
+      return -1;
     }
- 
-    while(fscanf(fp, "%[^,],%d\n", na, &po) != EOF)  //End Of File
-    {
-        insert(na, po, &head);
+
+    /* initialize */
+    head_nmodr = NULL;
+    head_ptodr = NULL;
+
+    while(fscanf(fp, "%d,%s", &point, name) != EOF) {
+      insert_nameorder(name, point, &head_nmodr);
     }
-    printf("\n");
- 
-    print_nodes(head);
- 
+
+    printf("***name order***\n");
+    show_list(head_nmodr);
+
+    move_nodes_pointorder(head_nmodr, &head_ptodr);
+
+    printf("***point order***\n");
+    show_list(head_ptodr);
+    
     fclose(fp);
-     
-    return 0;
+    free_list(head_nmodr);
+    free_list(head_ptodr);
+  }
+
+  return 0;
+}
+
+/**
+ * allocate memory and return new node.
+ */
+node_t *new_node(char *nm, int pnt, node_t *p)
+{
+  node_t *tmp;
+
+  tmp = (node_t *)malloc(sizeof(node_t));
+  
+  if (tmp == NULL) {
+    printf("Failed to allocate memory...\n");
+    return NULL;
+  }
+
+  tmp->name = (char *)malloc(sizeof(char) * strlen(nm));
+
+  if (tmp->name == NULL) {
+    printf("Failed to allocate memory...\n");
+    return NULL;
+  } else {
+    strcpy(tmp->name, nm);
+    tmp->point = pnt;
+    tmp->next = p;
+    return tmp;
+  }
+}
+
+/**
+ * insert a node to the linear list in order of name.
+ */
+int insert_nameorder(char *nm, int pnt, node_t **pp)
+{
+  node_t *tmp;
+  tmp = new_node(nm, pnt, NULL);
+
+  if (tmp == NULL) {
+    return FAILURE;
+  }
+
+  if (*pp == NULL)  {
+    *pp = tmp;
+  } else {
+    while((*pp) != NULL && (strcmp(tmp->name, (*pp)->name)) > 0 ) {
+      pp = &((*pp)->next);
+    }
+    tmp->next = *pp;
+    *pp = tmp;
+  }
+
+  return SUCCESS;
+}
+
+/**
+ * move nodes from a linear list which is sorted by name 
+ * to another linear list in order of point.
+ */
+int move_nodes_pointorder(node_t *p_nmodr, node_t **pp_ptodr)
+{
+  node_t *tmp;
+  while (p_nmodr != NULL) {
+
+    tmp = new_node(p_nmodr->name, p_nmodr->point, NULL);
+    if (tmp == NULL) {
+      return FAILURE;
+    }
+
+    if (*pp_ptodr == NULL) {
+      *pp_ptodr = tmp;
+    } else {
+      while ((*pp_ptodr) != NULL && ( tmp->point < (*pp_ptodr)->point )) {
+        pp_ptodr = &((*pp_ptodr)->next);
+      }
+      tmp->next = *pp_ptodr;
+      *pp_ptodr = tmp;
+    }
+
+    p_nmodr = p_nmodr->next;
+  }
+
+  return SUCCESS;
+}
+
+/**
+ * show all nodes in linear list.
+ */
+void show_list(node_t *p)
+{
+  while(p != NULL)
+  {
+    printf("name:%s,Point:%d\n", p->name, p->point);
+    p = p->next;
+  }
+}
+
+/**
+ * free memory of the list.
+ */
+void free_list(node_t *p)
+{
+  node_t *q;
+
+  while (p != NULL) {
+    q = p;
+    p = p->next;
+    free(q);
+  }
 }
