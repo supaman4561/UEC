dc925a1610b8e3c4067aef9a8cedc82181fdd59e
dosu416 <dosu416@gmail.com>
2019-11-07 08:55:22 +0900

    [UPDATE] delete redundant tmp

--- term_6/object/2nd/search_sim.m
+++ term_6/object/2nd/search_sim.m
@@ -3,8 +3,7 @@ function [sorted, index, url] = search_sim(N)
     query=db{N,2};
     sim = [];
     for i=1:size(db, 1)
-        tmp = db{i, 2};
-        sim = [sim intersection(tmp, query)];
+        sim = [sim intersection(db{i, 2}, query)];
     end
     [sorted, index] = sort(sim, 'descend');
     url = db(index, 1);
