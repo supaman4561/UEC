a80810ec59e6f8ea8086958c3a68834eb3024685
dosu416 <dosu416@gmail.com>
2019-11-07 08:33:24 +0900

    [UPDATE] modified the way of reference of cell array

--- term_6/object/2nd/search_sim.m
+++ term_6/object/2nd/search_sim.m
@@ -1,10 +1,10 @@
 function [sorted, index, url] = search_sim(N)
     load('db.mat');
-    query=db(N,2);
+    query=db{N,2};
     sim = [];
     for i=1:size(db, 1)
-        tmp = db(i, 2);
-        sim = [sim intersection(tmp{:}, query{:})];
+        tmp = db{i, 2};
+        sim = [sim intersection(tmp, query)];
     end
     [sorted, index] = sort(sim, 'descend');
     url = db(index, 1);
