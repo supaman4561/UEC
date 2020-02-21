f9d2c7955eeb35d65990c687a4899696771af378
dosu416 <dosu416@gmail.com>
2020-02-21 11:23:53 +0900

    [UPDATE] accを出力するように変更

--- term_6/object/final/report1/nn.m
+++ term_6/object/final/report1/nn.m
@@ -1,6 +1,6 @@
 % nearest neighbor method
 % 各引数は配列とする
-function nIdx = nn(train_p, train_n, test_p, test_n)
+function acc = nn(train_p, train_n, test_p, test_n)
     train = [train_p; train_n];
     test = [test_p; test_n];
     
@@ -12,4 +12,11 @@ function nIdx = nn(train_p, train_n, test_p, test_n)
     % 最近傍のindexを取得
     dist_mat = reshape(dist, size(train, 1), size(test, 1));
     [~,nIdx] = min(dist_mat);
+    
+    % 認識率の計算
+    border = size(train_p, 1);
+    nIdxP = nIdx(1:size(test_p, 1));                % ポジティブの分類先Index
+    nIdxN = nIdx((size(test_p, 1)+1):numel(nIdx));  % ネガティブの分類先Index
+    total = size(test, 1);
+    acc = (sum(nIdxP <= border) + sum(nIdxN > border)) / total;  % TP+TN/total
 end
