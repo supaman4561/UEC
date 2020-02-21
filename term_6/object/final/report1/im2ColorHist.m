1fc102279626fa4b588653b5dbff4bfe64fac0cd
dosu416 <dosu416@gmail.com>
2020-02-21 11:23:34 +0900

    [UPDATE] コメントの形式が間違っていたので修正

--- term_6/object/final/report1/im2ColorHist.m
+++ term_6/object/final/report1/im2ColorHist.m
@@ -1,13 +1,13 @@
 function ch = im2ColorHist(I)
-  // 各チャネルの要素を取得
+  % 各チャネルの要素を取得
   red = I(:, :, 1);
   green = I(:, :, 2);
   blue = I(:, :, 3);
 　
-  // 64色に減色
+  % 64色に減色
   X64 = floor(double(red)/64)*4*4 + floor(double(green)/64)*4 + floor(double(blue)/64);
 
-  // カラーヒストグラムの作成
+  % カラーヒストグラムの作成
   X64_v = reshape(X64, 1, numel(X64));
   ch = histcounts(X64_v, [0:63]);
 end
